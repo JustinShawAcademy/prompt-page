abe80870f1bf1c83db80b965d648b5e140e92b04
Justin Shaw <justinshawcareer@gmail.com>
2026-02-27 15:09:29 -0500

    feat: Add HeroSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import HeroSection from '@/components/ui/HeroSection'
 import React from 'react'
 
 const page = () => {
   return (
-    <>
-    </>
+    <main className='wrapper container'>
+      <HeroSection />
+    </main>
   )
 }
 
